5efd3cc3896463de46cd0e5d6a4445726c3facd3
info2000 <jose@turecarga.com>
2022-06-21 19:03:50 +0200

    Updated simply-static-3-1655830882

--- wp-content/plugins/elementor-pro/assets/js/popup.5ddbdd46f21fc221d760.bundle.js
+++ wp-content/plugins/elementor-pro/assets/js/popup.5ddbdd46f21fc221d760.bundle.js
@@ -1,14 +1,61 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fpopup.5ddbdd46f21fc221d760.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2Nywic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjcsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvcG9wdXAuNWRkYmRkNDZmMjFmYzIyMWQ3NjAuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.IEyadBR36zzrzNz6Oi3IgXTTjeOy1RGVdRujnRM4ot7uR_Pz16jrIP2_gPC3TtiuzV1GFP0hRh3EFtLSbpq75pPgPcoFYDk11xWBsSZJ2b7fP_GUecTED7_2-TNnkwHU_nN-TvQYRSdz0lSLbvZ2nvGQM6qkKb5IxvaRSGHQDH32lXRbRNa7a_o7eu6bxU_Onfq9U05kMTuLZ9xECBalzaWot0tSB_nEgeahhpfVnFSd5vHGqd2j4iAlT2CIaBir4Dv_fEB29Ed4Y5WGrzhYrv4BpQXL7TFPQDM8dCjA8PcJBayJ4udzl7cOVo4Vf9_WJnGWXv0SG19PN_3k3GlsOw">
-	</head>
-	<body>
-		<script type="text/javascript">
-			window.location = "https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fpopup.5ddbdd46f21fc221d760.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2Nywic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjcsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvcG9wdXAuNWRkYmRkNDZmMjFmYzIyMWQ3NjAuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.IEyadBR36zzrzNz6Oi3IgXTTjeOy1RGVdRujnRM4ot7uR_Pz16jrIP2_gPC3TtiuzV1GFP0hRh3EFtLSbpq75pPgPcoFYDk11xWBsSZJ2b7fP_GUecTED7_2-TNnkwHU_nN-TvQYRSdz0lSLbvZ2nvGQM6qkKb5IxvaRSGHQDH32lXRbRNa7a_o7eu6bxU_Onfq9U05kMTuLZ9xECBalzaWot0tSB_nEgeahhpfVnFSd5vHGqd2j4iAlT2CIaBir4Dv_fEB29Ed4Y5WGrzhYrv4BpQXL7TFPQDM8dCjA8PcJBayJ4udzl7cOVo4Vf9_WJnGWXv0SG19PN_3k3GlsOw";
-		</script>
+/*! elementor-pro - v3.4.1 - 01-09-2021 */
+(self["webpackChunkelementor_pro"] = self["webpackChunkelementor_pro"] || []).push([["popup"],{
 
-		<p>You are being redirected to <a href="https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fpopup.5ddbdd46f21fc221d760.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2Nywic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjcsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvcG9wdXAuNWRkYmRkNDZmMjFmYzIyMWQ3NjAuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.IEyadBR36zzrzNz6Oi3IgXTTjeOy1RGVdRujnRM4ot7uR_Pz16jrIP2_gPC3TtiuzV1GFP0hRh3EFtLSbpq75pPgPcoFYDk11xWBsSZJ2b7fP_GUecTED7_2-TNnkwHU_nN-TvQYRSdz0lSLbvZ2nvGQM6qkKb5IxvaRSGHQDH32lXRbRNa7a_o7eu6bxU_Onfq9U05kMTuLZ9xECBalzaWot0tSB_nEgeahhpfVnFSd5vHGqd2j4iAlT2CIaBir4Dv_fEB29Ed4Y5WGrzhYrv4BpQXL7TFPQDM8dCjA8PcJBayJ4udzl7cOVo4Vf9_WJnGWXv0SG19PN_3k3GlsOw">https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fpopup.5ddbdd46f21fc221d760.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2Nywic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjcsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvcG9wdXAuNWRkYmRkNDZmMjFmYzIyMWQ3NjAuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.IEyadBR36zzrzNz6Oi3IgXTTjeOy1RGVdRujnRM4ot7uR_Pz16jrIP2_gPC3TtiuzV1GFP0hRh3EFtLSbpq75pPgPcoFYDk11xWBsSZJ2b7fP_GUecTED7_2-TNnkwHU_nN-TvQYRSdz0lSLbvZ2nvGQM6qkKb5IxvaRSGHQDH32lXRbRNa7a_o7eu6bxU_Onfq9U05kMTuLZ9xECBalzaWot0tSB_nEgeahhpfVnFSd5vHGqd2j4iAlT2CIaBir4Dv_fEB29Ed4Y5WGrzhYrv4BpQXL7TFPQDM8dCjA8PcJBayJ4udzl7cOVo4Vf9_WJnGWXv0SG19PN_3k3GlsOw</a></p>
-	</body>
-</html>
+/***/ "../modules/popup/assets/js/frontend/handlers/forms-action.js":
+/*!********************************************************************!*\
+  !*** ../modules/popup/assets/js/frontend/handlers/forms-action.js ***!
+  \********************************************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.default = void 0;
+
+var _default = elementorModules.frontend.handlers.Base.extend({
+  getDefaultSettings() {
+    return {
+      selectors: {
+        form: '.elementor-form'
+      }
+    };
+  },
+
+  getDefaultElements() {
+    var selectors = this.getSettings('selectors'),
+        elements = {};
+    elements.$form = this.$element.find(selectors.form);
+    return elements;
+  },
+
+  bindEvents() {
+    this.elements.$form.on('submit_success', this.handleFormAction);
+  },
+
+  handleFormAction(event, response) {
+    if ('undefined' === typeof response.data.popup) {
+      return;
+    }
+
+    const popupSettings = response.data.popup;
+
+    if ('open' === popupSettings.action) {
+      return elementorProFrontend.modules.popup.showPopup(popupSettings);
+    }
+
+    setTimeout(() => {
+      return elementorProFrontend.modules.popup.closePopup(popupSettings, event);
+    }, 1000);
+  }
+
+});
+
+exports.default = _default;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=popup.5ddbdd46f21fc221d760.bundle.js.map
