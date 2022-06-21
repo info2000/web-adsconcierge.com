decfea42fdba2885c1c6509a9f7eaeea7d9d1cf7
info2000 <jose@turecarga.com>
2022-06-21 19:04:00 +0200

    Updated simply-static-3-1655830882

--- wp-content/plugins/elementor-pro/assets/js/nav-menu.e248eec66bc3d5587cf6.bundle.js
+++ wp-content/plugins/elementor-pro/assets/js/nav-menu.e248eec66bc3d5587cf6.bundle.js
@@ -1,14 +1,191 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fnav-menu.e248eec66bc3d5587cf6.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2OCwic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjgsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvbmF2LW1lbnUuZTI0OGVlYzY2YmMzZDU1ODdjZjYuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.N6iw4UlZFpT6vLxUY9OpCcyh6zPovrCtV4Y-VWlaS32c09CyOULWGXR8B6n4ozxRtbVXxE_5SlJU9Tw9f6U-XcvK632YCCHu9HPnNF7fCTRIZVmmG6qn8GMfuCThInuM0vucRyfU4oizRGFV2YxTvru-9aL2Cagkh1nwaZLWQmYyntPPkXA4yozsqJ8jp6zGVzjsJZ295ykLm0nbJM5yCrR_inqC2HTHMm9hO89A69lYELBu3KWmBqjuhLSRhLvyDqtGCU_QnvvJZApSiOdR61RjSVuzczvMMn7-YeBPu68VQeX6nP0RX0KdmVNClIRq4NW-nUUaBlv94P0UBfrjYQ">
-	</head>
-	<body>
-		<script type="text/javascript">
-			window.location = "https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fnav-menu.e248eec66bc3d5587cf6.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2OCwic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjgsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvbmF2LW1lbnUuZTI0OGVlYzY2YmMzZDU1ODdjZjYuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.N6iw4UlZFpT6vLxUY9OpCcyh6zPovrCtV4Y-VWlaS32c09CyOULWGXR8B6n4ozxRtbVXxE_5SlJU9Tw9f6U-XcvK632YCCHu9HPnNF7fCTRIZVmmG6qn8GMfuCThInuM0vucRyfU4oizRGFV2YxTvru-9aL2Cagkh1nwaZLWQmYyntPPkXA4yozsqJ8jp6zGVzjsJZ295ykLm0nbJM5yCrR_inqC2HTHMm9hO89A69lYELBu3KWmBqjuhLSRhLvyDqtGCU_QnvvJZApSiOdR61RjSVuzczvMMn7-YeBPu68VQeX6nP0RX0KdmVNClIRq4NW-nUUaBlv94P0UBfrjYQ";
-		</script>
+/*! elementor-pro - v3.4.1 - 01-09-2021 */
+(self["webpackChunkelementor_pro"] = self["webpackChunkelementor_pro"] || []).push([["nav-menu"],{
 
-		<p>You are being redirected to <a href="https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fnav-menu.e248eec66bc3d5587cf6.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2OCwic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjgsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvbmF2LW1lbnUuZTI0OGVlYzY2YmMzZDU1ODdjZjYuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.N6iw4UlZFpT6vLxUY9OpCcyh6zPovrCtV4Y-VWlaS32c09CyOULWGXR8B6n4ozxRtbVXxE_5SlJU9Tw9f6U-XcvK632YCCHu9HPnNF7fCTRIZVmmG6qn8GMfuCThInuM0vucRyfU4oizRGFV2YxTvru-9aL2Cagkh1nwaZLWQmYyntPPkXA4yozsqJ8jp6zGVzjsJZ295ykLm0nbJM5yCrR_inqC2HTHMm9hO89A69lYELBu3KWmBqjuhLSRhLvyDqtGCU_QnvvJZApSiOdR61RjSVuzczvMMn7-YeBPu68VQeX6nP0RX0KdmVNClIRq4NW-nUUaBlv94P0UBfrjYQ">https://thesocial.cloudflareaccess.com/cdn-cgi/access/login/socialads.adsconcierge.com?kid=f5780cff80e3a23790886593af9d298550ba50cba0b1461f7a6464f0a6c27be3&redirect_url=%2Fwp-content%2Fplugins%2Felementor-pro%2Fassets%2Fjs%2Fnav-menu.e248eec66bc3d5587cf6.bundle.js&meta=eyJraWQiOiJiYzY4ZTEyMzJmNjM5NjRlYjhhNWMxMWI4OWUxZmZlMWE5YWEyZDAwYzE4YTcyZTk0ZTkxZGFmNDAyNzg0MTNlIiwiYWxnIjoiUlMyNTYiLCJ0eXAiOiJKV1QifQ.eyJzZXJ2aWNlX3Rva2VuX3N0YXR1cyI6ZmFsc2UsImlhdCI6MTY1NTgyNjc2OCwic2VydmljZV90b2tlbl9pZCI6IiIsImF1ZCI6ImY1NzgwY2ZmODBlM2EyMzc5MDg4NjU5M2FmOWQyOTg1NTBiYTUwY2JhMGIxNDYxZjdhNjQ2NGYwYTZjMjdiZTMiLCJob3N0bmFtZSI6InNvY2lhbGFkcy5hZHNjb25jaWVyZ2UuY29tIiwidHlwZSI6Im1ldGEiLCJuYmYiOjE2NTU4MjY3NjgsInJlZGlyZWN0X3VybCI6Ilwvd3AtY29udGVudFwvcGx1Z2luc1wvZWxlbWVudG9yLXByb1wvYXNzZXRzXC9qc1wvbmF2LW1lbnUuZTI0OGVlYzY2YmMzZDU1ODdjZjYuYnVuZGxlLmpzIiwiaXNfZ2F0ZXdheSI6ZmFsc2UsImlzX3dhcnAiOmZhbHNlLCJhdXRoX3N0YXR1cyI6Ik5PTkUifQ.N6iw4UlZFpT6vLxUY9OpCcyh6zPovrCtV4Y-VWlaS32c09CyOULWGXR8B6n4ozxRtbVXxE_5SlJU9Tw9f6U-XcvK632YCCHu9HPnNF7fCTRIZVmmG6qn8GMfuCThInuM0vucRyfU4oizRGFV2YxTvru-9aL2Cagkh1nwaZLWQmYyntPPkXA4yozsqJ8jp6zGVzjsJZ295ykLm0nbJM5yCrR_inqC2HTHMm9hO89A69lYELBu3KWmBqjuhLSRhLvyDqtGCU_QnvvJZApSiOdR61RjSVuzczvMMn7-YeBPu68VQeX6nP0RX0KdmVNClIRq4NW-nUUaBlv94P0UBfrjYQ</a></p>
-	</body>
-</html>
+/***/ "../modules/nav-menu/assets/js/frontend/handlers/nav-menu.js":
+/*!*******************************************************************!*\
+  !*** ../modules/nav-menu/assets/js/frontend/handlers/nav-menu.js ***!
+  \*******************************************************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", ({
+  value: true
+}));
+exports.default = void 0;
+
+var _default = elementorModules.frontend.handlers.Base.extend({
+  stretchElement: null,
+
+  getDefaultSettings() {
+    return {
+      selectors: {
+        menu: '.elementor-nav-menu',
+        anchorLink: '.elementor-nav-menu--main .elementor-item-anchor',
+        dropdownMenu: '.elementor-nav-menu__container.elementor-nav-menu--dropdown',
+        menuToggle: '.elementor-menu-toggle'
+      }
+    };
+  },
+
+  getDefaultElements() {
+    var selectors = this.getSettings('selectors'),
+        elements = {};
+    elements.$menu = this.$element.find(selectors.menu);
+    elements.$anchorLink = this.$element.find(selectors.anchorLink);
+    elements.$dropdownMenu = this.$element.find(selectors.dropdownMenu);
+    elements.$dropdownMenuFinalItems = elements.$dropdownMenu.find('.menu-item:not(.menu-item-has-children) > a');
+    elements.$menuToggle = this.$element.find(selectors.menuToggle);
+    elements.$links = elements.$dropdownMenu.find('a.elementor-item');
+    return elements;
+  },
+
+  bindEvents() {
+    if (!this.elements.$menu.length) {
+      return;
+    }
+
+    this.elements.$menuToggle.on('click', this.toggleMenu.bind(this));
+
+    if (this.getElementSettings('full_width')) {
+      this.elements.$dropdownMenuFinalItems.on('click', this.toggleMenu.bind(this, false));
+    }
+
+    elementorFrontend.addListenerOnce(this.$element.data('model-cid'), 'resize', this.stretchMenu);
+  },
+
+  initStretchElement() {
+    this.stretchElement = new elementorModules.frontend.tools.StretchElement({
+      element: this.elements.$dropdownMenu
+    });
+  },
+
+  toggleNavLinksTabIndex(enabled = true) {
+    this.elements.$links.attr('tabindex', enabled ? 0 : -1);
+  },
+
+  toggleMenu(show) {
+    var isDropdownVisible = this.elements.$menuToggle.hasClass('elementor-active');
+
+    if ('boolean' !== typeof show) {
+      show = !isDropdownVisible;
+    }
+
+    this.elements.$menuToggle.attr('aria-expanded', show);
+    this.elements.$dropdownMenu.attr('aria-hidden', !show);
+    this.elements.$menuToggle.toggleClass('elementor-active', show);
+    this.toggleNavLinksTabIndex(show);
+
+    if (show && this.getElementSettings('full_width')) {
+      this.stretchElement.stretch();
+    }
+  },
+
+  followMenuAnchors() {
+    var self = this;
+    self.elements.$anchorLink.each(function () {
+      if (location.pathname === this.pathname && '' !== this.hash) {
+        self.followMenuAnchor(jQuery(this));
+      }
+    });
+  },
+
+  followMenuAnchor($element) {
+    const anchorSelector = $element[0].hash;
+    let offset = -300,
+        $anchor;
+
+    try {
+      // `decodeURIComponent` for UTF8 characters in the hash.
+      $anchor = jQuery(decodeURIComponent(anchorSelector));
+    } catch (e) {
+      return;
+    }
+
+    if (!$anchor.length) {
+      return;
+    }
+
+    if (!$anchor.hasClass('elementor-menu-anchor')) {
+      var halfViewport = jQuery(window).height() / 2;
+      offset = -$anchor.outerHeight() + halfViewport;
+    }
+
+    elementorFrontend.waypoint($anchor, function (direction) {
+      if ('down' === direction) {
+        $element.addClass('elementor-item-active');
+      } else {
+        $element.removeClass('elementor-item-active');
+      }
+    }, {
+      offset: '50%',
+      triggerOnce: false
+    });
+    elementorFrontend.waypoint($anchor, function (direction) {
+      if ('down' === direction) {
+        $element.removeClass('elementor-item-active');
+      } else {
+        $element.addClass('elementor-item-active');
+      }
+    }, {
+      offset,
+      triggerOnce: false
+    });
+  },
+
+  stretchMenu() {
+    if (this.getElementSettings('full_width')) {
+      this.stretchElement.stretch();
+      this.elements.$dropdownMenu.css('top', this.elements.$menuToggle.outerHeight());
+    } else {
+      this.stretchElement.reset();
+    }
+  },
+
+  onInit() {
+    elementorModules.frontend.handlers.Base.prototype.onInit.apply(this, arguments);
+
+    if (!this.elements.$menu.length) {
+      return;
+    }
+
+    const elementSettings = this.getElementSettings(),
+          iconValue = elementSettings.submenu_icon.value;
+    let subIndicatorsContent = '';
+
+    if (iconValue) {
+      // The value of iconValue can be either className inside the editor or a markup in the frontend.
+      subIndicatorsContent = iconValue.indexOf('<') > -1 ? iconValue : `<i class="${iconValue}"></i>`;
+    } // subIndicators param - Added for backwards compatibility:
+    // If the old 'indicator' control value = 'none', the <span class="sub-arrow"> wrapper element is removed
+
+
+    this.elements.$menu.smartmenus({
+      subIndicators: '' !== subIndicatorsContent,
+      subIndicatorsText: subIndicatorsContent,
+      subIndicatorsPos: 'append',
+      subMenusMaxWidth: '1000px'
+    });
+    this.initStretchElement();
+    this.stretchMenu();
+
+    if (!elementorFrontend.isEditMode()) {
+      this.followMenuAnchors();
+    }
+  },
+
+  onElementChange(propertyName) {
+    if ('full_width' === propertyName) {
+      this.stretchMenu();
+    }
+  }
+
+});
+
+exports.default = _default;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=nav-menu.e248eec66bc3d5587cf6.bundle.js.map
